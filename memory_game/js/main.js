1ea4689f22396e34a244dffdb05548436f60e9cc
Carly Marsh <carlymarsh@Carlys-MacBook-Pro.local>
2019-01-15 15:45:24 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,24 +24,39 @@ var cardsInPlay = [];
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match");
-	} else {
-	console.log("Sorry, try again.");
+		alert("You found a match!");
+	}	else {
+		alert("Sorry, try again.");
 	}
 };
+	
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
-if (cardsInPlay.legth === 2) {
-	checkForMatch();
-}
-
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 };
 
-flipCard(0);
-flipCard(2);
-flipCard(1);
-flipCard(3);
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement =  document.createElement("img");
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+
+
+createBoard();
+
+
+
